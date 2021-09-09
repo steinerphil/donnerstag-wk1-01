808003a7998ed890c55a8e4023c3f4fb68c16175
Philipp Steiner <philipp-steiner@gmx.de>
2021-09-09 12:55:43 +0200

    the alarmstufe is now choosable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int numberOfPersons = 31;
-        checkPersons(numberOfPersons);
+        int numberOfPersons = 0;
+        String alarmstufe = "rot";
+        checkPersons(numberOfPersons, alarmstufe);
     }
 
 
-public static boolean checkPersons(int count){
-        if(count <= 30){
+public static boolean checkPersons(int count, String alarmstufe){
+        int maximumNumberOfPersons = 0;
+        switch (alarmstufe){
+            case "rot":
+                maximumNumberOfPersons = 0;
+                break;
+            case "gelb":
+                maximumNumberOfPersons = 30;
+                break;
+            case "grün":
+                maximumNumberOfPersons = 60;
+                break;
+            default:
+                System.out.println("Falsche Eingabe [Alarmstufe]");
+                return false;
+        }
+        if(count <= maximumNumberOfPersons){
             System.out.println("Maximale Personenanzahl nicht überschritten.");
             return true;
         }
